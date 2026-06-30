82f4402942986c1d069bab714b077bc55c715559
verwann <erwann.varlet@ynov.com>
2026-06-30 14:03:21 +0200

    feat: add android build dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.androidApplication)
@@ -21,6 +22,14 @@ dependencies {
 
     implementation("io.insert-koin:koin-android:4.1.+")
 }
+
+// Read API key from local.properties
+val localProperties = Properties() //import java.utils
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+
 
 android {
     namespace = "com.amonteiro.a06_ynov_kmp"
@@ -46,5 +55,34 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    defaultConfig {
+        //Mettre une valeur plus élevée à chaque mise à jour
+        versionCode = 1
+        //N'importe quelle chaîne de caractères
+        versionName = "0.5-beta"
+    }
+
+    //Récupérer les informations du keystore dans local.properties pour signer l'exécutable de sortie
+    //A mettre avant buildTypes
+    signingConfigs {
+        val keystoreFile = localProperties.getProperty("KEYSTORE_FILE")
+        if (!keystoreFile.isNullOrBlank()) {
+            create("release") {
+                // rootProject.file -> chemin résolu depuis la racine (où est le keystore)
+                storeFile = rootProject.file(keystoreFile)
+                storePassword = localProperties.getProperty("KEYSTORE_PASSWORD")
+                keyAlias = localProperties.getProperty("KEY_ALIAS")
+                keyPassword = localProperties.getProperty("KEY_PASSWORD")
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.findByName("release")
+        }
     }
 }
